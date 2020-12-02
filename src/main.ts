d41a7fabeb5373477527bc76b164ab7ec4e19de3
Aakash Srivastav <akhsri21@gmail.com>
2020-12-03 00:05:47 +0530

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Personal Budget Tracker')
+    .setDescription('The APIs for Budget Tracker')
+    .setVersion('1.0')
+    .addTag('budget-tracker')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
