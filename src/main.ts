464c5315d60e40230be923223d31800edb667f8f
akhsri <akhsri21@gmail.com>
2022-01-22 15:47:54 +0530

    Change: port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  await app.listen(3004);
 }
 bootstrap();
